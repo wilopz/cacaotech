f8331372982cd12e03f314f1b760a624c7d12503
olarte-droid <dagov1998@gmail.com>
2022-02-02 21:19:52 -0500

    app en 4 pantallas

---
+++ screens/analize.js
@@ -0,0 +1,13 @@
+import { React } from 'react'
+import {StyleSheet,  View, Text } from 'react-native'
+
+
+const analize = () => {
+    return(
+        <View>
+            <Text>Analize</Text>
+        </View>
+    )
+}
+
+export default analize
---
+++ screens/historic.js
@@ -0,0 +1,18 @@
+import { React } from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+
+const historic = () => {
+    return(
+        <View>
+            <Text>Historic</Text>
+        </View>
+    )
+}
+
+export default historic
+
+
+
+
+
