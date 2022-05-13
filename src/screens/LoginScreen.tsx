11bcbbb193bfe6c285368975a1b8fc9f684281f8
Dani <odos26@hotmail.com>
2022-05-13 12:57:31 -0500

    Camara y galeria Ok

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,23 +1,56 @@
-import React from 'react'
-import { View, Text, ImageBackground, StyleSheet, TouchableOpacity } from 'react-native';
-import { PanelCp } from '../components/PanelCp';
-import { ButtonCp } from '../components/ButtonCp';
+import React, { useContext, useState } from 'react'
+import { View, Text, ImageBackground, StyleSheet, TouchableOpacity, Dimensions, Image } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+
+
 
 interface Props extends StackScreenProps<any,any>{};
 
+const { width, height } = Dimensions.get('window');
+
+
 export const LoginScreen = ( {navigation}:Props ) => {
+  
+  const [ tempUri, setTempUri ] = useState<string>()
+  
+  const img = '../images/camera_icon.png'
+  
+  const takePhoto = () => {
+    launchCamera({
+      mediaType: 'photo',
+    }, ( respuesta ) => {
+      if ( respuesta.didCancel ) return;
+      if ( !respuesta.uri ) return;
+
+      setTempUri( respuesta.uri );
+      console.log( respuesta);
+    });
+  }
+
+  const takePhotoFromGallery = () => {
+    launchImageLibrary({
+      mediaType: 'photo',
+    }, ( respuesta ) => {
+      if ( respuesta.didCancel ) return;
+      if ( !respuesta.uri ) return;
+
+      setTempUri( respuesta.uri );
+      console.log( respuesta);
+    });
+  }
+
+
+
   return (
     <View style={styles.container}>
+
         <ImageBackground
         style={styles.imagescreen}
         source={ require('../images/screen2.jpg')}
         />
 
         <View style={styles.panelCp}>
-        </View> 
-
-        <View style={styles.textos}>
           <Text style = {[
                   styles.text, 
                   styles.textTittle,    
@@ -31,34 +64,25 @@ export const LoginScreen = ( {navigation}:Props ) => {
               ]}>
             Adjunta aqui las fotografias de tus frutos para que podamos identificarlas
           </Text>
-        </View>
 
-        <TouchableOpacity style={styles.red}>
-          <ImageBackground
-          style={styles.cameraIcon}
-          source={ require('../images/camera_icon.png')}
-          />
-        </TouchableOpacity>
+          <TouchableOpacity
+            onPress={ takePhoto }
+          >
+            <ImageBackground
+            style={styles.cameraIcon}
+            source={ require('../images/camera_icon.png')}
+            />
+          </TouchableOpacity>
 
-        
+          <TouchableOpacity
+            onPress={ takePhotoFromGallery }
+          >
+            <ImageBackground
+            style={styles.imagesIcon}
+            source={ require('../images/images_icon.png')}
+            />
+          </TouchableOpacity>
 
-        <TouchableOpacity style={styles.red}>
-          <Text style = {[
-                styles.text, 
-                styles.textGray
-              ]}>
-            Adjunta aqui las fotografias de tus frutos para que podamos identificarlas
-          </Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity>
-          <ImageBackground
-          style={styles.imagesIcon}
-          source={ require('../images/images_icon.png')}
-          />
-        </TouchableOpacity>
-        
-        <View style={ styles.botones }>
           <TouchableOpacity 
             style={styles.buttonCp}
             onPress={ () => navigation.navigate('LoginScreen')}
@@ -68,28 +92,40 @@ export const LoginScreen = ( {navigation}:Props ) => {
 
           <TouchableOpacity 
             style={styles.buttonCp}
-            onPress={ () => navigation.navigate('HistoryScreen')}
+            onPress={ () => navigation.navigate('LoginScreen')}
           > 
-            <Text style= { styles.textButton }>{'Historial de envios'}</Text>
+            <Text style= { styles.textButton }>{'Ver resultados'}</Text>
           </TouchableOpacity> 
 
           <TouchableOpacity 
             style={styles.buttonCp}
-            onPress={ () => navigation.navigate('LoginScreen')}
+            onPress={ () => navigation.navigate('HistoryScreen')}
           > 
-            <Text style= { styles.textButton }>{'Ver resultados'}</Text>
+            <Text style= { styles.textButton }>{'Historial de envios'}</Text>
           </TouchableOpacity>  
+        
+          {
+            ( tempUri ) && (
+              <Image
+                source={{ uri: tempUri }}
+                style={{
+                  width: 80,
+                  height: 100,
+                  bottom: 300,
+                  left: 50,
+                }}
+              />
+            )
+          }
+          
 
         </View>
-        
+
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  red: {
-    backgroundColor: 'red'
-  },
   container:{
     flex: 1,
     alignSelf: 'flex-start'
@@ -98,19 +134,15 @@ const styles = StyleSheet.create({
     width: 428,
     height: 224,
   },
-  panelCp: {
-    height:'78%',
+  panelCp:{
     backgroundColor: 'white',
+    height: height*0.75,
+    width: width,
+    position: 'absolute',
+    bottom: 0,
     borderTopLeftRadius: 50,
     borderTopRightRadius: 50,
-    bottom: 50,
-    width: 410,
-    alignContent: 'center',
-  },
-  textos: {
-    height: 200, 
-    bottom: '84%',
-  },
+},
   text: {
     fontFamily: 'Mulish',
     alignSelf: 'center',
@@ -118,44 +150,36 @@ const styles = StyleSheet.create({
   textTittle:{
     fontSize: 30,
     fontWeight: "bold",
-    bottom: -10,
+    marginTop: 20,
     color: 'black',
   },
   text3:{
-      fontSize: 16,
-      bottom: -20,
-      color: 'black',
-      textAlign: 'center',
-      width: 350
-  },
-  cameraIcon: {
-      width: 150,
-      height: 150,
-      bottom: 735,
-      alignSelf: 'center',
-  },
-  imagesIcon: {
-      width: 70,
-      height: 70,
-      bottom: 725,
-      alignSelf: 'center',
-  },
-  textGray: {
     fontSize: 16,
-    bottom: -150,
+    bottom: -20,
     color: 'black',
     textAlign: 'center',
-    width: 350,
-    position:'absolute'
+    width: 350
   },
-  botones: {
-    flex: 1,
-    width: '100%',
-    height: '30%',
-    position: 'absolute',
-    bottom: 5,
-    justifyContent: 'space-evenly'
-
+  cameraIcon: {
+    width: 150,
+    height: 150,
+    alignSelf: 'center',
+    marginTop: 40,
+  },
+  imagesIcon: {
+    width: 70,
+    height: 70,
+    alignSelf: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  buttonCp: {
+    width: width*0.8, 
+    height: height*0.06,
+    backgroundColor: '#2094FE',
+    alignSelf: 'center',
+    borderRadius: 10,
+    marginTop: 10
   },
   textButton: {
     fontSize: 16,
@@ -165,12 +189,4 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: 'center',
   },
-  buttonCp: {
-    width: '80%', 
-    height: '22%',
-    backgroundColor: '#2094FE',
-    alignSelf: 'center',
-    borderRadius: 10,
-  },
-
 })
